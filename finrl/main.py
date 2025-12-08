93172de940f68f00ffb59f12dfab74fa5215edf8
Mike Licitra <michael.licitra@gmail.com>
2025-12-08 11:36:19 -0500

    updated gitignore to stop tracking config_private.py

--- finrl/main.py
+++ finrl/main.py
@@ -22,12 +22,12 @@ from finrl.meta.env_stock_trading.env_stocktrading_np import StockTradingEnv
 
 # construct environment
 
-# try:
-#     from finrl.config_private import ALPACA_API_KEY, ALPACA_API_SECRET
-# except ImportError:
-#     raise FileNotFoundError(
-#         "Please set your own ALPACA_API_KEY and ALPACA_API_SECRET in config_private.py"
-#     )
+try:
+    from finrl.config_private import ALPACA_API_KEY, ALPACA_API_SECRET
+except ImportError:
+    raise FileNotFoundError(
+        "Please set your own ALPACA_API_KEY and ALPACA_API_SECRET in config_private.py"
+    )
 
 
 def build_parser():
@@ -61,19 +61,21 @@ def main() -> int:
         env = StockTradingEnv
 
         # demo for elegantrl
-        kwargs = (
-            {}
-        )  # in current meta, with respect yahoofinance, kwargs is {}. For other data sources, such as joinquant, kwargs is not empty
+        kwargs ={} 
+            # in current meta, with respect yahoofinance, kwargs is {}. For other data sources, such as joinquant, kwargs is not empty
         train(
             start_date=TRAIN_START_DATE,
             end_date=TRAIN_END_DATE,
             ticker_list=DOW_30_TICKER,
-            data_source="yahoofinance",
-            time_interval="1D",
+            data_source="alpaca",
+            time_interval="1Min",
             technical_indicator_list=INDICATORS,
             drl_lib="elegantrl",
             env=env,
             model_name="ppo",
+            API_KEY=ALPACA_API_KEY,
+            API_SECRET=ALPACA_API_SECRET,
+            API_BASE_URL=ALPACA_API_BASE_URL,
             cwd="./test_ppo",
             erl_params=ERL_PARAMS,
             break_step=1e5,
@@ -86,18 +88,21 @@ def main() -> int:
 
         # demo for elegantrl
         # in current meta, with respect yahoofinance, kwargs is {}. For other data sources, such as joinquant, kwargs is not empty
-        kwargs = {}
+        kwargs ={} 
 
         account_value_erl = test(  # noqa
             start_date=TEST_START_DATE,
             end_date=TEST_END_DATE,
             ticker_list=DOW_30_TICKER,
-            data_source="yahoofinance",
-            time_interval="1D",
+            data_source="alpaca",
+            time_interval="1Min",
             technical_indicator_list=INDICATORS,
             drl_lib="elegantrl",
             env=env,
             model_name="ppo",
+            API_KEY=ALPACA_API_KEY,
+            API_SECRET=ALPACA_API_SECRET,
+            API_BASE_URL=ALPACA_API_BASE_URL,
             cwd="./test_ppo",
             net_dimension=512,
             kwargs=kwargs,
@@ -112,13 +117,12 @@ def main() -> int:
                 "Please set your own ALPACA_API_KEY and ALPACA_API_SECRET in config_private.py"
             )
         env = StockTradingEnv
-        kwargs = {}
         trade(
             start_date=TRADE_START_DATE,
             end_date=TRADE_END_DATE,
             ticker_list=DOW_30_TICKER,
-            data_source="yahoofinance",
-            time_interval="1D",
+            data_source="alpaca",
+            time_interval="1Min",
             technical_indicator_list=INDICATORS,
             drl_lib="elegantrl",
             env=env,
